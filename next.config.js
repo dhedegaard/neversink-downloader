fec7b992a252ada5b792046a81ef90d44407eada
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-10-13 22:50:18 +0200

    SW: Added clientsClaim, skipWaiting and inlineWorkboxRuntime.

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    clientsClaim: true,
+    skipWaiting: true,
+    inlineWorkboxRuntime: true,
   },
 });
